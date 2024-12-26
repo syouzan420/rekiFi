dd2b3acf8efe4634a523f446ab6b6b63675974cf
syouzan420 <syouzanponkikki@yahoo.co.jp>
2024-12-26 23:08:44 +0900

    update

--- Main.hs
+++ Main.hs
@@ -10,9 +10,24 @@ main :: IO ()
 main = do
   bmps <- setBmps
   Just ce <- elemById "canvas"
+  Just bup <- elemById "up"
+  Just bleft <- elemById "left"
+  Just bok <- elemById "ok"
+  Just bright <- elemById "right"
+  Just bdown <- elemById "down"
   Just c <- fromElem ce
   ci <- getCanvasInfo c
   state <- newIORef initState 
+  onEvent bok Click $ \_ -> do 
+    readIORef state >>= inputLoop c ci bmps 32 >>= writeIORef state
+  onEvent bup Click $ \_ -> do 
+    readIORef state >>= inputLoop c ci bmps 107 >>= writeIORef state
+  onEvent bleft Click $ \_ -> do 
+    readIORef state >>= inputLoop c ci bmps 104 >>= writeIORef state
+  onEvent bright Click $ \_ -> do 
+    readIORef state >>= inputLoop c ci bmps 108 >>= writeIORef state
+  onEvent bdown Click $ \_ -> do 
+    readIORef state >>= inputLoop c ci bmps 106 >>= writeIORef state
   onEvent document KeyDown $ \(KeyData kc _ _ _ _) -> do
     preventDefault
     readIORef state >>= inputLoop c ci bmps kc >>= writeIORef state
